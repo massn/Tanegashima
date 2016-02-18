f148b18306926bceb281cee96d4ae70b7ba77245
Massn <massn23579@gmail.com>
2016-02-19 02:23:24 +0900

    tag v0.0.3

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ The package can be installed as:
   * Add tanegashima to your list of dependencies in `mix.exs`:
 
         def deps do
-          [{:tanegashima, "~> 0.0.2"}]
+          [{:tanegashima, "~> 0.0.3"}]
         end
 
 ## Usage
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Tanegashima.Mixfile do
   use Mix.Project
   def project do
     [app: :tanegashima,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
